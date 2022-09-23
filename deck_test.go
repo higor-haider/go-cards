28f990c4107c09a5eedcd750d3e2398da50ac0e7
higor-haider <higor.haider@sumup.com>
2022-09-23 20:51:39 -0300

    Adds more validations to new deck test

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Error("Expected deck length of 16, but got", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("Expected first card of Ace of Spades, but got", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Error("Expected last card of Four of Clubs, but got", d[len(d)-1])
+	}
 }
